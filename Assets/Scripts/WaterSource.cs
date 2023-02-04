59aaee848dcfb066f36505cb450ece57b18c8efd
Churner <78268614+Churner@users.noreply.github.com>
2023-02-04 01:11:33 +0100

    water source maxed and positive

--- Assets/Scripts/WaterSource.cs
+++ Assets/Scripts/WaterSource.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class WaterSource : MonoBehaviour
 {
     public float waterAmount = 10f;
+    public float maxAmount = 100f;
 
     public void UseWater(float amount)
     {
         waterAmount -= amount;
+        if (waterAmount < 0)
+        {
+            waterAmount = 0;
+        }
+    }
+    public void refillWater(float amount)
+    {
+        waterAmount += amount;
+        if (waterAmount > maxAmount)
+        {
+            waterAmount = maxAmount;
+        }
     }
 }
