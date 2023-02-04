cc4f23fbad6b7c75f95ea58a0553d58f6847d75e
Churner <78268614+Churner@users.noreply.github.com>
2023-02-05 00:14:25 +0100

    seed UI

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
     public int Seeds;
+    public TMP_Text seedCounterText;
+
+    public void Start()
+    {
+        Seeds = 0;
+        UpdateSeedCounter();
+    }
+
+    public void UpdateSeedCounter()
+    {
+        seedCounterText.text = "" + Seeds;
+    }
 
     public void AddSeeds(int amount)
     {
         Seeds += amount;
+        UpdateSeedCounter();
         GlobalEvents.Instance.SendEvent(GlobalEventEnum.OnGainSeed, ReferencesSingleton.Instance.player.Inventory.Seeds);
     }
 
     public void RemoveSeeds(int amount)
     {
         Seeds -= amount;
+        UpdateSeedCounter();
         if (Seeds < 0)
             Seeds = 0;
     }
