fa35c37d2aa2a408949a830df7e7d3671d052ee4
Sébastien Damiens <sebdami@hotmail.fr>
2023-02-04 15:49:57 +0100

    Water Shader

---
+++ Assets/WaterReceptacle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterReceptacle : MonoBehaviour
+{
+    public enum CheckOptionEnum
+    {
+        Trigger,
+        Collision
+    }
+
+    public CheckOptionEnum CheckOption;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
