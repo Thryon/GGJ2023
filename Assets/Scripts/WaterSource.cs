e126b90661b4f2d4340cbad6f1604427acb518e4
Churner <78268614+Churner@users.noreply.github.com>
2023-02-04 01:50:36 +0100

    water level based on amount

--- Assets/Scripts/WaterSource.cs
+++ Assets/Scripts/WaterSource.cs
@@ -6,18 +6,24 @@ public class WaterSource : MonoBehaviour
 {
     public float waterAmount = 10f;
     public float maxAmount = 100f;
+    public float maxLvl = 0f;
+    public float minLvl = -3f;
+    private Transform WaterTransform;
 
     public void UseWater(float amount)
     {
         waterAmount -= amount;
+        WaterTransform.localPosition = new Vector3(WaterTransform.localPosition.x, Mathf.Lerp(maxLvl, minLvl, waterAmount / maxAmount), WaterTransform.localPosition.z);
         if (waterAmount < 0)
         {
             waterAmount = 0;
         }
+
     }
-    public void refillWater(float amount)
+    public void RefillWater(float amount)
     {
         waterAmount += amount;
+        WaterTransform.localPosition = new Vector3(WaterTransform.localPosition.x, Mathf.Lerp(maxLvl, minLvl, waterAmount / maxAmount), WaterTransform.localPosition.z);
         if (waterAmount > maxAmount)
         {
             waterAmount = maxAmount;
