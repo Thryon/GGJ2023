c4acbca9263a66efa556da39b8dea6f72fa5a149
Churner <78268614+Churner@users.noreply.github.com>
2023-02-05 14:17:36 +0100

    UpgradeMenu

--- Assets/UpgradeMenu.cs
+++ Assets/UpgradeMenu.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UpgradeMenu : MonoBehaviour
 {
     [SerializeField] private GameObject container;
+    [SerializeField] private TMP_Text UpgradeText;
     // Start is called before the first frame update
     void Start()
     {
         container.SetActive(false);
+        UpgradeText.gameObject.SetActive(true);
         GlobalEvents.Instance.RegisterEvent(GlobalEventEnum.OpenUpgradeMenu, Open);
         GlobalEvents.Instance.RegisterEvent(GlobalEventEnum.CloseUpgradeMenu, Close);
     }
@@ -16,13 +19,16 @@ public class UpgradeMenu : MonoBehaviour
     private void Close()
     {
         container.SetActive(false);
+        UpgradeText.gameObject.SetActive(true);
         GlobalEvents.Instance.SendEvent(GlobalEventEnum.OnUpgradeMenuClosed);
     }
 
     private void Open()
     {
         container.SetActive(true);
+        UpgradeText.gameObject.SetActive(false);
         GlobalEvents.Instance.SendEvent(GlobalEventEnum.OnUpgradeMenuOpened);
+
     }
 
     public void OnUpgradePowerClicked()
