1e7b2c7ca1165eafef7724db3f22deff7cccd786
OSG-Seb <Sebdami@hotmail.fr>
2023-02-06 19:43:53 +0100

    Fix backwards text

--- Assets/RotateTowardsPlayerY.cs
+++ Assets/RotateTowardsPlayerY.cs
@@ -7,5 +7,9 @@ public class RotateTowardsPlayerY : MonoBehaviour
     void Update()
     {
         transform.LookAt(Camera.main.transform, Vector3.up);
+        Vector3 rot = transform.localEulerAngles;
+        rot.y += 180f;
+        rot.x = 360f - rot.x;
+        transform.localEulerAngles = rot;
     }
 }
